b0b302476a2954d5111f846683441139d4dcc6a3
Emily York <emily@Emilys-MacBook-Pro.local>
2020-03-03 17:58:21 -0500

    initial commit

---
+++ challenge.js
@@ -0,0 +1,31 @@
+// function evenOdd(x,y) {
+// 	z = (x+y)
+// 	if ( z % 2 == 0) {
+// 	console.log( z + " is even") 
+// } else {
+// 		console.log (z + " is odd")
+// 	}}
+
+// function higherLower (x,y) {
+// 	z = (x/y) 
+// 		if (z > 0) {
+// 			console.log (x + " divided by " + y + " is greater than 0")
+// 		}
+// 		else if (z < 0) {
+// 			console.log (x + " divided by " + y + " is less than 0")
+
+// 		}
+// 		else {
+// 			console.log (x + " divided by " + y + " is 0")
+// 	}}
+
+
+function odd (array) {
+	for (var i = 0; i < array.length; i++) {
+	if (array[i] % 2 !== 0 ) {
+	console.log(array);
+	}}
+}
+
+
+
