56429b537f631d29bda812fb6867ff308f1e340e
Emily York <emily@Emilys-MacBook-Pro.local>
2020-03-03 18:10:43 -0500

    Write a function that takes three arguments - all numbers - adds them together, split the result in 3 and displays the result on the console.

--- challenge.js
+++ challenge.js
@@ -1,31 +1,7 @@
-// function evenOdd(x,y) {
-// 	z = (x+y)
-// 	if ( z % 2 == 0) {
-// 	console.log( z + " is even") 
-// } else {
-// 		console.log (z + " is odd")
-// 	}}
+// Write a function that takes three arguments - all numbers - adds them together, 
+// split the result in 3 and displays the result on the console.
 
-// function higherLower (x,y) {
-// 	z = (x/y) 
-// 		if (z > 0) {
-// 			console.log (x + " divided by " + y + " is greater than 0")
-// 		}
-// 		else if (z < 0) {
-// 			console.log (x + " divided by " + y + " is less than 0")
-
-// 		}
-// 		else {
-// 			console.log (x + " divided by " + y + " is 0")
-// 	}}
-
-
-function odd (array) {
-	for (var i = 0; i < array.length; i++) {
-	if (array[i] % 2 !== 0 ) {
-	console.log(array);
-	}}
+function challenge(x,y,z) {
+	var num = (x+y+z)/3;
+	console.log(num)
 }
-
-
-
